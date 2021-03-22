7278c793fc20512e8f8329f5d36df1c26535664d
Piotr Laszewski <laszpio@gmail.com>
2021-03-22 08:43:36 +0000

    Response.add/3 edge cases

--- lib/simple_agent/response.ex
+++ lib/simple_agent/response.ex
@@ -13,6 +13,10 @@ defmodule SimpleAgent.Response do
     Map.update!(response, k, &(&1 ++ [v]))
   end
 
+  def add(_, :memory, _), do: raise(ArgumentError, "memory must be a map")
+
+  def add(_, k, _), do: raise(ArgumentError, "#{k} must be a binary")
+
   def validate(%__MODULE__{} = response), do: {:ok, response}
 
   def validate(_response), do: :error
--- test/response_test.exs
+++ test/response_test.exs
@@ -62,7 +62,34 @@ defmodule SimpleAgentResponseTest do
       assert result.memory == %{first_key: "value", other_key: "other value"}
     end
 
-    test "raises exception when adding not existant key to response" do
+    test "reaise error when adding non-map memory to response" do
+      assert_raise ArgumentError, "memory must be a map", fn ->
+        add(@response, :memory, nil)
+      end
+
+      assert_raise ArgumentError, "memory must be a map", fn ->
+        add(@response, :memory, 'str')
+      end
+
+      assert_raise ArgumentError, "memory must be a map", fn ->
+        add(@response, :memory, 1)
+      end
+
+      assert_raise ArgumentError, "memory must be a map", fn ->
+        add(@response, :memory, [])
+      end
+    end
+
+    test "raises error when adding non-binary to list attributes" do
+      for list_attr <- [:errors, :logs, :messages],
+          bad_value <- [nil, 1, '', [], %{}] do
+        assert_raise ArgumentError, "#{list_attr} must be a binary", fn ->
+          add(@response, list_attr, bad_value)
+        end
+      end
+    end
+
+    test "raises error when adding not-existant key to response" do
       assert_raise KeyError, ~r/key :unknown not found in: %SimpleAgent.Response/, fn ->
         add(@response, :unknown, "value")
       end
